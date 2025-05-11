5e2655336a5f6fa2ce578360fcf76580b933d9c3
Mino Inoue <phungtuandat29052004@gmail.com>
2025-05-11 14:41:29 +0700

    Update Data.sql

--- Data.sql
+++ Data.sql
@@ -8,6 +8,29 @@ VALUES
     ('ad3', '123', N'Hoàng Việt Long', '2025-03-20 14:20:00');
 GO
 
+INSERT INTO CauLacBo (MaCauLacBo, TenCauLacBo, MoTaNgan, NoiDung, NgayCapNhat, UrlAnh, MaTheLoai, MaThanhVien)
+VALUES
+    (
+        1, 
+        N'Câu lạc bộ Truyền thông FIT Media',
+        N'Câu lạc bộ Truyền thông của Trường Công nghệ thông tin và Truyền thông, chuyên tổ chức các hoạt động truyền thông và sự kiện.',
+        N'Câu lạc bộ Truyền thông FIT Media thuộc Trường Công nghệ thông tin và Truyền thông (FIT) được thành lập với mục tiêu thúc đẩy các hoạt động truyền thông, quảng bá hình ảnh nhà trường, và tổ chức các sự kiện văn hóa, học thuật. CLB hoạt động trong các lĩnh vực như sản xuất nội dung truyền thông, quay dựng video, thiết kế đồ họa, và quản lý các kênh truyền thông xã hội. Tham gia FIT Media, sinh viên có cơ hội phát triển kỹ năng sáng tạo, làm việc nhóm, và xây dựng mạng lưới quan hệ.',
+        GETDATE(),
+        'https://example.com/fit-media-logo.jpg',
+        1,
+        1
+    ),
+    (
+        2,
+        N'Câu lạc bộ Thể thao điện tử Trường Công nghệ thông tin và Truyền thông HEC',
+        N'CLB dành cho những người yêu thích thể thao điện tử.',
+        N'CLB HEC được thành lập với mục đích mang bộ môn Esports đến gần hơn với sinh viên Trường Công nghệ thông tin và Truyền thông nói riêng và trường Đại học Công nghiệp nói chung. Phòng máy Esports Training Room của HEC được trang bị đầy đủ thiết bị thi đấu phục vụ cho các giải đấu esports chuyên nghiệp.',
+        GETDATE(),
+        'https://sict.haui.edu.vn/media/77/t77821.jpg',
+        1,
+        1
+    );
+	GO	
 
 -- Insert into TheLoai (Main categories)
 INSERT INTO TheLoai (MaTheLoai, TenTheLoai, VisibleTheLoai, VisibleTheLoai1, SapXep, Url, Target, LinkNgoai)
